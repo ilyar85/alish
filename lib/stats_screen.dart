b039810055a8c98ec036a96571b83672c5a45a45
User_B7K <ilyar85@gmail.com>
2023-07-01 13:26:52 +0000

    stat

--- lib/stats_screen.dart
+++ lib/stats_screen.dart
@@ -9,19 +9,22 @@ class StatsScreen extends StatefulWidget {
 }
 
 class _StatsScreenState extends State<StatsScreen> {
+  String _selectedPlan = '10';
+
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            _buildStatCard(),
-            _buildPlanCard(),
+            _buildStatCard(screenHeight * 0.3),
+            _buildPlanCard(screenHeight * 0.1),
             Row(
               children: [
-                Expanded(child: _buildErrorCard()),
-                Expanded(child: _buildCategoryCard()),
+                Expanded(child: _buildErrorCard(screenHeight * 0.4)),
+                Expanded(child: _buildCategoryCard(screenHeight * 0.4)),
               ],
             ),
           ],
@@ -30,134 +33,160 @@ class _StatsScreenState extends State<StatsScreen> {
     );
   }
 
-  Widget _buildStatCard() {
-    return Card(
-      color: Color(0xFF101010),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(38.0),
-      ),
-      margin: const EdgeInsets.all(10),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/eye.png',
-                  width: 50,
-                ),
-                Text(
-                  '100 очков', // общее количество очков
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontFamily: 'K_Gotham Pro',
+  Widget _buildStatCard(double height) {
+    return Container(
+      height: height,
+      child: Card(
+        color: Color(0xFF101010),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(38.0),
+        ),
+        margin: const EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/eye.png',
+                    width: 50,
                   ),
-                ),
-              ],
-            ),
-            Text(
-              'Пройдено',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontFamily: 'K_Gotham Pro',
-              ),
-            ),
-            Text(
-              '38/100', // текущая статистика
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 36,
-                fontFamily: 'K_Gotham Pro',
-              ),
-            ),
-            Text(
-              'Общее количество изученных слов',
-              style: TextStyle(
-                color: Color(0xFF6F6F6F),
-                fontSize: 14,
-                fontFamily: 'K_Gotham Pro',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: CircularPercentIndicator(
-                radius: 80.0,
-                lineWidth: 8.0,
-                percent: 0.38, // процент выполнения
-                center: Text(
-                  '38%', // процент выполнения
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
+                  SizedBox(width: 10),
+                  Text(
+                    '100', // общее количество очков
+                    style: TextStyle(
+                      color: Color(0xFF003AFF),
+                      fontSize: 24,
+                      fontFamily: 'K_Gotham Pro',
+                    ),
                   ),
-                ),
-                progressColor: Colors.green,
+                ],
               ),
-            ),
-          ],
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Пройдено',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'K_Gotham Pro',
+                        ),
+                      ),
+                      Text(
+                        '38/100', // текущая статистика
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontFamily: 'K_Gotham Pro',
+                        ),
+                      ),
+                      Text(
+                        'Общее количество изученных слов',
+                        style: TextStyle(
+                          color: Color(0xFF6F6F6F),
+                          fontSize: 14,
+                          fontFamily: 'K_Gotham Pro',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  CircularPercentIndicator(
+                    radius: 60.0, // уменьшил радиус
+                    lineWidth: 6.0,
+                    percent: 0.38, // процент выполнения
+                    center: Text(
+                      '38%', // процент выполнения
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    progressColor: Color(0xffffffff),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildPlanCard() {
-    return Card(
-      color: Color(0xFF101010),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(38.0),
-      ),
-      margin: const EdgeInsets.all(10),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Text(
-              'План',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontFamily: 'K_Gotham Pro',
+  Widget _buildPlanCard(double height) {
+    return Container(
+      height: height,
+      child: Card(
+        color: Color(0xFF101010),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(38.0),
+        ),
+        margin: const EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'количество',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontFamily: 'K_Gotham Pro',
+                    ),
+                  ),
+                  _buildPlanOption('5'),
+                  _buildPlanOption('10'),
+                  _buildPlanOption('15'),
+                ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildPlanOption('5'),
-                _buildPlanOption('10'),
-                _buildPlanOption('15'),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildPlanOption(String number) {
-    return ChoiceChip(
-      label: Text(
-        number,
-        style: TextStyle(
-          color: Colors.white,
-          fontFamily: 'K_Gotham Pro',
+    return Row(
+      children: [
+        Text(
+          number,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'K_Gotham Pro',
+          ),
         ),
-      ),
-      selected: number == '10',
-      onSelected: (bool selected) {
-        // Обновить выбранный план
-      },
-      selectedColor: Color(0xFF101010),
-      backgroundColor: Color(0xFF6F6F6F),
+        Radio<String>(
+          value: number,
+          groupValue: _selectedPlan,
+          onChanged: (String? value) {
+            setState(() {
+              _selectedPlan = value!;
+            });
+          },
+          activeColor: Colors.white,
+          fillColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.selected)) {
+              return Colors.white;
+            }
+            return Colors.white; // цвет кнопки когда она не выбрана
+          }),
+        ),
+      ],
     );
   }
 
-  Widget _buildErrorCard() {
-    return ConstrainedBox(
-      constraints: BoxConstraints(minHeight: 150),
+  Widget _buildErrorCard(double height) {
+    return Container(
+      height: height,
       child: Card(
         color: Color(0xFF101010),
         shape: RoundedRectangleBorder(
@@ -219,9 +248,9 @@ class _StatsScreenState extends State<StatsScreen> {
     );
   }
 
-  Widget _buildCategoryCard() {
-    return ConstrainedBox(
-      constraints: BoxConstraints(minHeight: 150),
+  Widget _buildCategoryCard(double height) {
+    return Container(
+      height: height,
       child: Card(
         color: Color(0xFF101010),
         shape: RoundedRectangleBorder(
